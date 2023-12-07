7c65abae6253a5f03ce78f2866c464af234997ba
MrT223 <148790555+MrT223@users.noreply.github.com>
2023-12-08 00:35:13 +0700

    first commit

---
+++ js/radius.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){ 	
+						   
+						   
+	// radius Box
+	$('.menu_nav ul li a').css({"border-bottom-left-radius": "8px", "-moz-border-radius-bottomleft":"8px", "-webkit-border-bottom-left-radius":"8px", "border-bottom-right-radius": "8px", "-moz-border-radius-bottomright":"8px", "-webkit-border-bottom-right-radius":"8px"});
+	// end radius Box
+	 
+});	
